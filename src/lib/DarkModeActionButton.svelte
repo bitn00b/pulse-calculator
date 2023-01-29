f902d0c67e980df96b67b5a70f489c45073158b4
bitn00b <109045767+bitn00b@users.noreply.github.com>
2023-01-29 15:15:25 +0100

    darkmode always on for now - until the switching works at all

---
+++ src/lib/DarkModeActionButton.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { ActionIcon } from '@svelteuidev/core';
+
+  import { isDarkMode } from "./settings.js";
+  import { Moon, Sun } from "radix-icons-svelte";
+</script>
+
+<ActionIcon variant="default" on:click={() => $isDarkMode = !$isDarkMode} size={30}>
+  {#if $isDarkMode}
+    <Moon/>
+  {:else}
+    <Sun/>
+  {/if}
+</ActionIcon>
