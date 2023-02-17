edc75fb26fb3646244ed332e1a1042ddb4aba198
bitn00b <109045767+bitn00b@users.noreply.github.com>
2023-02-17 23:49:04 +0100

    Refactor Table to its own component (the fixed header / scroll functionality)

--- src/lib/InterestDailyHistory.svelte
+++ src/lib/InterestDailyHistory.svelte
@@ -1,89 +1,59 @@
 <script lang="ts">
   import FormattedNumber from "./components/FormattedNumber.svelte";
-  import { days } from "./logic/store.js";
-  import type { IterationResult } from "./logic/interestForIterations";
-  import { onMount } from "svelte";
+  import FixedHeaderTable from "./components/FixedHeaderTable.svelte";
+  import type { IterationResult } from "./logic/types";
 
   export let iteration: IterationResult;
-
-  console.info('daily build');
-
-  onMount(() => {
-    console.info('on Mount');
-  })
 </script>
 
-
-<table style="width: 100%;" class="fixed-table-header">
-  <tr>
+<FixedHeaderTable data={iteration.interests}>
+  <svelte:fragment slot="headerRow">
     <th style="text-align: left">Day</th>
     <th style="text-align: left">Start</th>
     <th style="text-align: left">End</th>
     <th>Profit</th>
-  </tr>
-  <!-- actual data for the hidden row, to have the same-ish header widths -->
-  <tr style="opacity: 0; height: 0">
-    <td>{$days}</td>
-    <td>
-      <FormattedNumber number={iteration.interests[iteration.interests.length -1].startedWith}/>
+  </svelte:fragment>
+
+  <svelte:fragment slot="hiddenDataRow" let:data>
+
+    <td>{data.day}</td>
+    <td>$
+      <FormattedNumber number={data.startedWith}/>
     </td>
-    <td>
-      <FormattedNumber number={iteration.interests[iteration.interests.length -1].currentValue}/>
+    <td>$
+      <FormattedNumber number={data.currentValue}/>
     </td>
-    <td>
-      <FormattedNumber number={iteration.interests[iteration.interests.length -1].profitOfThisDay}/>
+    <td style="text-align: center">
+      $
+      <FormattedNumber number={data.profitOfThisDay}/>
     </td>
-  </tr>
-</table>
-<div class="scroll">
-  <table style="width: 100%;" class="scrolling-table background-rows">
-    <tr style="opacity: 0">
-      <th style="text-align: left">Day</th>
-      <th style="text-align: left">Start</th>
-      <th style="text-align: left">End</th>
-      <th>Profit</th>
+
+  </svelte:fragment>
+
+  {#each iteration.interests as dailyInterest, index (dailyInterest.day)}
+    <tr class={index % 2 ? 'odd': 'even'}>
+      <td>{dailyInterest.day}</td>
+      <td>$
+        <FormattedNumber number={dailyInterest.startedWith}/>
+      </td>
+      <td>$
+        <FormattedNumber number={dailyInterest.currentValue}/>
+      </td>
+      <td style="text-align: center">
+        $
+        <FormattedNumber number={dailyInterest.profitOfThisDay}/>
+      </td>
     </tr>
-    <tbody>
-    {#each iteration.interests as dailyInterest (dailyInterest.day)}
-      <tr>
-        <td>{dailyInterest.day}</td>
-        <td>$
-          <FormattedNumber number={dailyInterest.startedWith}/>
-        </td>
-        <td>$
-          <FormattedNumber number={dailyInterest.currentValue}/>
-        </td>
-        <td style="text-align: center">
-          $
-          <FormattedNumber number={dailyInterest.profitOfThisDay}/>
-        </td>
-      </tr>
-    {/each}
-    </tbody>
-  </table>
-</div>
+  {/each}
+</FixedHeaderTable>
+
 
 <style lang="scss">
-  .scroll {
-    height: 18rem;
-    overflow: auto
+  tr.even {
+    background: #AAAAAA60
   }
 
-  .fixed-table-header {
-    margin-bottom: -2rem;;
-  }
-
-  .scrolling-table {
-    margin-top: -2rem;
-  }
-
-  .background-rows {
-    tr:nth-child(even) {
-      background: #AAAAAA60
-    }
-
-    tr:nth-child(odd) {
-      background: #AAAAAA30
-    }
+  tr.odd {
+    background: #AAAAAA30
   }
 </style>
---
+++ src/lib/components/FixedHeaderTable.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let data: unknown[];
+</script>
+
+
+<table style="width: 100%;" class="fixed-table-header">
+  <tr>
+    <slot name="headerRow"></slot>
+  </tr>
+  <!-- actual data for the hidden row, to have the same-ish header widths -->
+  <tr style="opacity: 0; height: 0">
+    <slot name="hiddenDataRow" data={data[data.length -1]}></slot>
+  </tr>
+</table>
+<div class="scroll">
+  <table style="width: 100%;" class="scrolling-table background-rows">
+    <tr style="opacity: 0">
+      <slot name="headerRow"></slot>
+    </tr>
+    <tbody>
+      <slot></slot>
+    </tbody>
+  </table>
+</div>
+
+<style lang="scss">
+  .scroll {
+    height: 18rem;
+    overflow: auto
+  }
+
+  .fixed-table-header {
+    margin-bottom: -2rem;;
+  }
+
+  .scrolling-table {
+    margin-top: -2rem;
+  }
+</style>
